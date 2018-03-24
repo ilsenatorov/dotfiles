05ca5f88d632376791a4a9bb5b9409854faaa72c
ilsenatorov <il.senatorov@gmail.com>
2018-03-24 20:04:25 +0000

    lock script added

---
+++ wallpapers/lock.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+xkb-switch -s gb 
+icon="/home/ilya/dotfiles/wallpapers/lock.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -u -i "$tmpbg"
