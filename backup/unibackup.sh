a020f9fb6202952326558bc5f004ab6f38333936
ilsenatorov <il.senatorov@gmail.com>
2018-04-15 11:52:07 +0100

    minor changes

--- backup/unibackup.sh
+++ backup/unibackup.sh
@@ -3,6 +3,6 @@ cd /home/ilya/dotfiles/backup
 
 DATE=`date '+%Y/%m/%d %H:%M:%S'`
 echo "" >> ./unilogs.txt
-gdrive sync upload ~/Documents/ 12HX9Nwrr1MXECLbdsZYyUe42HAuR_wXu | tee -a ./unilogs.txt
+gdrive sync upload /home/ilya/Documents/ 12HX9Nwrr1MXECLbdsZYyUe42HAuR_wXu | tee -a ./unilogs.txt
 echo "the_date" $DATE >> ./unilogs.txt
 grep -A1 "Sync finished" ./unilogs.txt | grep -P -o "\d+\/\d+\/\d+ \d+:\d+:\d+" | tail -1 > ./.lastunibackup.txt
--- keyboard/change_colours.sh
+++ keyboard/change_colours.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-col_num='\[Color1Intense\]'
-
-colors=$(grep -A1 $col_num ~/.cache/wal/colors-konsole.colorscheme | grep -v $col_num | grep -o '[0-9]*,[0-9]*,[0-9]*' | sed 's/[ \t]*$//')
-msi-keyboard -m normal -r left,$colors -r middle,$colors -r right,$colors
+col_main='\[Color1Intense\]'
+colors_main=$(grep -A1 $col_main ~/.cache/wal/colors-konsole.colorscheme | grep -v $col_main | grep -o '[0-9]*,[0-9]*,[0-9]*' | sed 's/[ \t]*$//')
+msi-keyboard -m normal -r left,$colors_main -r middle,$colors_main -r right,$colors_main
