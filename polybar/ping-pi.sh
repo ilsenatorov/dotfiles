2c3cf8d37fad1225cc9ccca4ae9a80d3bb1899cd
ilsenatorov <il.senatorov@gmail.com>
2018-08-13 13:19:38 +0000

    polybar and stuff

---
+++ polybar/ping-pi.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+source ~/dotfiles/polybar/.openweathermap_key
+ping -W 1 -c 1 -s 1 $PI_IP > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "%{F#00aa00}⇄%{F-}"
+else
+  echo "%{F#aa0000}%{F-}"
+fi
