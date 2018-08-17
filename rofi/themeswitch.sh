486ac132828e05cecf11309e96bbb2867533cc1a
ilsenatorov <il.senatorov@gmail.com>
2018-08-17 22:42:05 +0100

    tray removed

---
+++ rofi/themeswitch.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+### Created by ilsenatorov
+### Change WALLPAPERDIR to the directory where you store wallpapers
+
+WALLPAPERDIR=/run/media/ilya/SD/Pictures/wallpapers/
+
+if [ -z $@ ]
+then
+function get_themes()
+{
+    ls $WALLPAPERDIR
+}
+echo current; get_themes
+else
+    THEMES=$@
+    if [ x"current" = x"${THEMES}" ]
+    then
+        exit 0
+        #wal -i `cat ~/.cache/wal/wal` > /dev/null
+    elif [ -n "${THEMES}" ]
+    then
+        wal -i $WALLPAPERDIR${THEMES} > /dev/null
+    fi
+fi
