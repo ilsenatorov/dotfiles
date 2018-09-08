41326b84cf96bbf2be1dd69e5e448aa0fd5d1b72
ilsenatorov <il.senatorov@gmail.com>
2018-09-08 13:33:26 +0300

    backup fix

--- backup/borg_backup.sh
+++ backup/borg_backup.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 source /home/ilya/dotfiles/.sensitive
-BORG_OPTS="--stats --one-file-system --compression lz4 --checkpoint-interval 86400"
-export BORG_RELOCATED_REPO_ACCESS_IS_OK=no
-export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=no
 DATE=$(date +'%y-%m-%d_%a_%H:%M:%S')-$(hostname)
-TARGET=pi:/mnt/backups/big
-
+### CREATE BACKUP
 borg create $BORG_OPTS \
   --exclude /root/.cache \
   --exclude /var/cache \
@@ -16,11 +12,13 @@ borg create $BORG_OPTS \
   --exclude /lost+found \
   --exclude /tmp \
   $TARGET::$DATE-$$-system \
-  / /boot >> $BACKUPDIR/full.log
+  / /boot
+### PRUNE ARCHIVES
 borg prune \
 -v \
 --list \
 --keep-last 50 \
 --keep-daily 30 \
---keep-monthly 24 $TARGET >> $BACKUPDIR/full.log
+--keep-monthly 24 $TARGET
+
 /home/ilya/dotfiles/backup/fetch_borg_list.sh
--- backup/fetch_borg_list.sh
+++ backup/fetch_borg_list.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 source /home/ilya/dotfiles/.sensitive
-BACKUPDIR=/home/ilya/dotfiles/backup
-TARGET='pi:/mnt/backups/big'
 LIST=$(borg list $TARGET)
 echo $LIST > $BACKUPDIR/borg.log
 cat $BACKUPDIR/borg.log | grep -Po '\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d' | tail -n 1 > $BACKUPDIR/.lastbackup.txt
