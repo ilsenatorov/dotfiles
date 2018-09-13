d7c99d391dfe828ec27da4f72677bf02e7dc509d
ilsenatorov <il.senatorov@gmail.com>
2018-09-13 21:47:28 +0300

    small readability improvement

--- polybar/LB.sh
+++ polybar/LB.sh
@@ -3,9 +3,10 @@ t=0
 toggle() {
     t=$(((t + 1) % 2))
     }
+trap "toggle" USR1
 
 function check_run () {
-    local stat=$(ps aux | grep -c $1)
+local stat=$(ps aux | grep -c $1)
 if [ $stat -eq 0 ]; then
 	echo "%{u$color1 +u}LB"
 else
@@ -13,7 +14,6 @@ else
 fi
 }
 
-trap "toggle" USR1
 while true; do
     if [ $t -eq 0 ]; then
         check_run "[b]org_backup"
