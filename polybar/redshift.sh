240304d18e36b6db0297ffe196151818365898c2
ilsenatorov <il.senatorov@gmail.com>
2018-10-31 19:06:19 +0100

    added a redshift script to polybar

---
+++ polybar/redshift.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+. ~/.cache/wal/colors.sh
+# Get the gamma values from xrandr. When redshift isn't on, all values are 1.0.
+GAMMAX=$(xrandr --verbose | grep 'Gamma' | awk -F ':' '{print $2}' | tr -d ' ')
+GAMMAY=$(xrandr --verbose | grep 'Gamma' | awk -F ':' '{print $3}' | tr -d ' ')
+GAMMAZ=$(xrandr --verbose | grep 'Gamma' | awk -F ':' '{print $4}' | tr -d ' ')
+
+# Check for at least one value not being 1.0. X appears to stay as 1.0, but Y and Z change.
+if [ $GAMMAX != 1.0 ] | [ $GAMMAY != 1.0 ] | [ $GAMMAZ != 1.0 ]; then
+	ICON="%{u$color5 +u}"
+else
+	ICON="%{u$color1 +u}"
+fi 
+
+echo $ICON
