0855a823606f675653dafceacdd2bf715ce360d0
ilsenatorov <il.senatorov@gmail.com>
2019-05-14 21:32:55 +0200

    added pulling

---
+++ backup/pull_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+source ~/dotfiles/.sensitive
+if [[ $1 == '--home' ]]; then
+    IP=$PI_HOME_IP
+else
+    IP=$PI_IP
+fi
+rsync -av --delete pi@$IP:/mnt/ftp/storage/Documents ~/
--- polybar/ping-pi.sh
+++ polybar/ping-pi.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 source ~/dotfiles/.sensitive
 . ~/.cache/wal/colors.sh
-nc -z $PI_IP 22 > /dev/null 2>&1
+if [[ $1 == '--home' ]]; then
+    IP=$PI_HOME_IP
+else
+    IP=$PI_IP
+fi
+nc -z $IP 22 > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "%{u$color1 +u}π: %{F#00aa00}⇄%{F-}%{u#0000ff}"
 else
