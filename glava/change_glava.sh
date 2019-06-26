b5411ca95b048eea4cb66fb8f8afb25200f0af5b
ilsenatorov <il.senatorov@gmail.com>
2019-06-26 16:36:22 +0200

    added glava files

--- glava/change_glava.sh
+++ glava/change_glava.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-color=$(sed -n 3p ~/.cache/wal/colors)
+color=$(sed -n 10p ~/.cache/wal/colors)
 sed -i "s|#define COLOR.*$|#define COLOR ("$color" * ((d / 60) + 1))|g" ~/.config/glava/bars.glsl
 sed -i "s|#define COLOR.*$|#define COLOR ("$color" * ((d / 40) + 1))|g" ~/.config/glava/radial.glsl
